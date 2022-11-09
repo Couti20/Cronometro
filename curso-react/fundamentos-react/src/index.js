ed175f1c70021dabcaa0eac216ecd6bf7ea265ff
Michael Coutinho <michaelcoutyy@gmail.com>
2022-11-09 19:57:22 -0300

    atualizado

---
+++ curso-react/fundamentos-react/src/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+import Reactqualquenome from 'react-dom'
+import React from 'react'
+
+
+
+const tag =  <strong>Bora la React!!!</strong>
+
+
+Reactqualquenome.render(
+<div>
+    {tag}
+</div>,
+document.getElementById('root')
+)
